352ec0a552a721e658826fc60b7240647417cc56
piero ds <dslpiero@gmail.com>
2017-03-31 19:03:37 +0200

    added folder tree

--- gitmarks.py
+++ gitmarks.py
@@ -13,10 +13,16 @@ handlers = [ofh]
 dispatcher = dispatcher.Dispatcher(handlers)
 
 vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
+hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=3)
+
 main_window.add(vbox)
 
-header_bar = widgets.MyHeaderBar(dispatcher).make_headerbar("Gitmarks")
+header_bar = widgets.HeaderBar(dispatcher).make_headerbar("Gitmarks")
 main_window.set_titlebar(header_bar)
+
+tree_view = widgets.FolderTree().make_tree()
+hbox.pack_start(tree_view, True, True, 0)
+vbox.pack_start(hbox, True, False, 0)
 vbox.pack_end(status_bar, False, True, 0)
 main_window .connect("delete-event", Gtk.main_quit)
 main_window .show_all()
--- widgets.py
+++ widgets.py
@@ -1,10 +1,10 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, Gdk, GdkPixbuf
 
 import dispatcher
 
-class MyHeaderBar:
+class HeaderBar:
 
   def __init__(self, dispatcher:dispatcher.Dispatcher):
     self.dispatcher = dispatcher
@@ -22,3 +22,33 @@ class MyHeaderBar:
 
   def open_msg_handler(self, widget):
     self.dispatcher.dispatch("open_button", "open_file")
+
+
+class FolderTree:
+
+  def __init__(self):
+    self.tree_view = Gtk.TreeView()
+    self.model = Gtk.TreeStore(str, GdkPixbuf.Pixbuf)
+    self.icon = Gtk.IconTheme.get_default().load_icon("folder", 24, 0)
+
+
+  def make_tree(self):
+    # None = append to root
+    last_added = self.model.append(None, ["Folder 1", self.icon])
+    self.model.append(last_added, ["Folder One.One", self.icon])
+
+    self.tree_view.set_model(self.model)
+
+    self.folder_name_renderer = Gtk.CellRendererText()
+    self.folder_icon_renderer = Gtk.CellRendererPixbuf()
+
+    self.column = Gtk.TreeViewColumn("Folder Name", self.folder_name_renderer, text=0)
+    self.column = Gtk.TreeViewColumn("Folder Name")
+    self.column.pack_start(self.folder_icon_renderer, False)
+    self.column.pack_start(self.folder_name_renderer, True)
+    self.column.add_attribute(self.folder_icon_renderer, "pixbuf", 1)
+    self.column.add_attribute(self.folder_name_renderer, "text", 0)
+
+    self.tree_view.append_column(self.column)
+
+    return self.tree_view
